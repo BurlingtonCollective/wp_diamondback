9aa5b76b829340acc8612cf05d0011567e125678
Zachary Lincoln <zach@burlingtoncollective.com>
2014-11-15 05:02:42 -0500

    events done

--- home.php
+++ home.php
@@ -114,38 +114,51 @@ endwhile; endif;
 				?>
 			</div>
 			<div class="col-xs-12 col-md-6">
+				<?php
+				$eventsQuery = new WP_Query(array('post_type'=>'events', 'posts_per_page'=>3, 'orderby'=>'meta_value', 'meta_key'=>'wpcf-date'));
+				if($eventsQuery->have_posts()):
+				?>
 				<div class="page-header">
 					<h2>Upcoming Events <a href="/upcoming-events" class="btn btn-default pull-right">See all events</a></h2>
 				</div>
 				<ul class="media-list">
-					<li class="media">
+					<?php
+					while($eventsQuery->have_posts()): $eventsQuery->the_post();
+					?>
+					<li class="media clearfix">
 				    <a class="pull-left" href="#">
-				      <img class="media-object" src="<?= get_template_directory_uri(); ?>/images/events/rhg.jpg">
+				    	<?php
+				    	if(has_post_thumbnail()):
+				    	?>
+				    	<?php the_post_thumbnail('full', array('class'=>'media-object')); ?>
+				      <!-- <img class="media-object" src="<?= get_template_directory_uri(); ?>/images/events/rhg.jpg"> -->
+				      <?php
+				      else:
+				      ?>
+
+
+				    	<?php
+				    	endif;
+				    	?>
 				    </a>
 				    <div class="media-body">
-				      <h4 class="media-heading">Sour Hour III &middot; 11/27/14</h4>
-				      <p>Diamondback Beer will be sponsoring a Thanksgiving morning event at The Rowhouse Grille in Federal Hill.  $15 gets you an all-you-can-eat breakfast buffet and bottomless Diamondback Beer until 10am.  Contact us at <a href="mailto:info@diamondbackbeer.com">info@diamondbackbeer.com</a> for further information.</p>
+				    	<?php
+				    	$eventDate = types_render_field('date', array('format'=>'m/d/y'));
+				    	$dateString = $eventDate ? $eventDate : 'TBD';
+				    	?>
+				      <h4 class="media-heading"><?php the_title(); ?> &middot; <?php echo $dateString; ?></h4>
+				      <div class="wysiwyg-content">
+				      	<?php the_content(); ?>
+				      </div>
 				    </div>
 				  </li>
-				  <li class="media">
-				    <a class="pull-left" href="#">
-				      <img class="media-object" src="<?= get_template_directory_uri(); ?>/images/events/brewers-cask.jpg">
-				    </a>
-				    <div class="media-body">
-				      <h4 class="media-heading">Promotional Night &middot; TBD</h4>
-				      <p>Diamondback Beer will host a promo night at The Brewer's Cask in Federal Hill.  Stop by and try our 3:30 Amber Ale as well as chat with the owners and brewers!  Date is TBD, so check back soon for more information.</p>
-				    </div>
-				  </li>
-				  <li class="media">
-				    <a class="pull-left" href="#">
-				      <img class="media-object" src="<?= get_template_directory_uri(); ?>/images/events/diamondback.jpg">
-				    </a>
-				    <div class="media-body">
-				      <h4 class="media-heading">Promotional Night &middot; TBD</h4>
-				      <p>Check back for more information on our upcoming promo nights.</p>
-				    </div>
-				  </li>
+				  <?php
+				  endwhile;
+				  ?>
 				</ul>
+				<?php
+				endif;
+				?>
 			</div>
 		</div>
 	</div>
